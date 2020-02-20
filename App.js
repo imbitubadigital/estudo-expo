f42772d6a4b230952e52e87322862c720fe8f840
Tony Fernandes <imbitubadigital@gmail.com>
2020-02-20 18:03:38 -0300

    criando páginas

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default function App() {
             color="white"
           />
         }
-        title="Entrar"
+        title="Entrar Agora"
       />
     </View>
   );
---
+++ src/pages/Restaurants/Top/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Top() {
+  return (
+    <View>
+      <Text>Topo</Text>
+    </View>
+  );
+}
