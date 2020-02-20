878cdb1a668aa3f3d5e7cb00724251a8b1880ae7
Tony Fernandes <imbitubadigital@gmail.com>
2020-02-20 17:32:57 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button, Icon } from 'react-native-elements';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Studos!</Text>
+      <Button
+        icon={
+          <Icon
+            type="material-community"
+            name="check-outline"
+            size={15}
+            color="white"
+          />
+        }
+        title="Entrar"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
